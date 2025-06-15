b27b137891cd0ea1734306ae6c87b77f46567363
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-06-15 21:09:17 +0200

    Quick login mock-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,44 @@
 
 import styles from "@/styles/mainPage.module.css";
 import { useRouter } from "next/navigation";
-import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
-  let isAdmin = false;
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleLogin = () => {
-    router.refresh();
-    if (!isAdmin){
-      //signIn('azure-ad', {callbackUrl: '/userMain'});
+    if (password.includes("admin")) {
+      router.push("/adminMain");
+    } else {
       router.push("/userMain");
     }
-    else {
-      router.push("/adminMain");
-    };
-  }
-  const flipAdmin = () => {
-    isAdmin = !isAdmin;
-  }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.logo}>📘</div>
       <h1 className={styles.title}>Zmitac 2.0</h1>
+
+      <input
+        className={styles.loginInput}
+        type="text"
+        placeholder="Login"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        className={styles.loginInput}
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
       <button className={styles.loginButton} onClick={handleLogin}>
         Login
       </button>
-      <input className={styles.checkAdminButton} type="checkbox" id="admin" name="admin" onClick={flipAdmin} />
-      <label htmlFor="admin">Admin?</label>
     </div>
   );
 }
